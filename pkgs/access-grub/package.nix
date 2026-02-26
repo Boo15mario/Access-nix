0de66ab34583035e97cd98cb06a0fa81b9c7fa33
boo15mario <bala.alek@gmail.com>
2026-02-25 23:17:06 -0600

    Change access-grub to fetch from GitHub repo

--- pkgs/access-grub/package.nix
+++ pkgs/access-grub/package.nix
@@ -1,14 +1,19 @@
-{ stdenv, lib }:
+{ stdenv, lib, fetchFromGitHub }:
 
 stdenv.mkDerivation {
   pname = "access-grub";
   version = "1.0";
 
-  src = ./red-white-grub;
+  src = fetchFromGitHub {
+    owner = "Boo15mario";
+    repo = "access-grub";
+    rev = "main";
+    sha256 = "1y6yvkvvak6s22iiyqrbyg60z8r2xh7nqnq83qhfas7y0x7k7ank";
+  };
 
   installPhase = '
     mkdir -p $out
-    cp -r ./* $out/
+    cp -r ./red-white-grub/* $out/
   ';
 
   meta = with lib; {
