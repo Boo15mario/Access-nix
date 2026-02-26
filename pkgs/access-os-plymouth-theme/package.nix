598de316f735d3d1eb9d69d33b058b0051f6860a
boo15mario <bala.alek@gmail.com>
2026-02-26 15:12:56 -0600

    Add access-os-plymouth-theme package

---
+++ pkgs/access-os-plymouth-theme/package.nix
@@ -0,0 +1,25 @@
+{ stdenv, lib, fetchFromGitHub }:
+
+stdenv.mkDerivation {
+  pname = "access-os-plymouth-theme";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "boo15mario";
+    repo = "access-os-plymouth-theme";
+    rev = "main";
+    sha256 = "0s4dc970ikw445hxa2p4da25g77rrb4lp4ix6wzivigqfd0mywvz";
+  };
+
+  installPhase = '
+    mkdir -p $out/share/plymouth/themes
+    cp -r access-os-boot $out/share/plymouth/themes/
+    # Fix paths in .plymouth file if necessary (usually they are relative to the theme directory)
+  ';
+
+  meta = with lib; {
+    description = "Official Plymouth boot splash theme for access-OS.";
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
+}
